87d13faf403ee7025b896592f700718c22ef829b
Gabriela Vespoli <gabiat3@hotmail.com>
2021-02-17 12:20:24 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StarRating from './starRating';
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating/>
+    </div>
+  );
+}
+
+export default App;
